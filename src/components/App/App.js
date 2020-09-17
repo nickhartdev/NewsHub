7fbcd6a41b46276b2049ac6e331969e577cdc5de
Nick Hart <nick.hart.des@gmail.com>
2020-09-17 12:12:37 -0500

    Refactor App to class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import LanguageSelect from '../LanguageSelect/LanguageSelect';
 import InterestSelect from '../InterestSelect/InterestSelect';
 require('dotenv').config();
 
-function App() {
-  return (
-    <div className="App">
-      <Route path="/language-select">
-        <LanguageSelect />
-      </Route>
-      <Route path="/interest-select">
-        <InterestSelect />
-      </Route>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Route path="/language-select">
+          <LanguageSelect />
+        </Route>
+        <Route path="/interest-select">
+          <InterestSelect />
+        </Route>
+      </div>
+    )
+  };
 }
 
 export default App;
