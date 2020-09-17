263862a4e8a70f01d807132030acfacac95921d9
Nick Hart <nick.hart.des@gmail.com>
2020-09-17 14:32:33 -0500

    Set up i18n config

--- src/i18n.js
+++ src/i18n.js
@@ -2,3 +2,18 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-18next';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false
+    }
+  })
+
+export default i18n;
+
