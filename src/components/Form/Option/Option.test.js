88aa1eb5b7ca7c6f8d579f57fb716b11ec154be6
Nick Hart <nick.hart.des@gmail.com>
2020-09-17 21:02:26 -0500

    Fix option test

--- src/components/Form/Option/Option.test.js
+++ src/components/Form/Option/Option.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; 
 import { render, screen } from '@testing-library/react';
 import Option from './Option';
 
 describe('Option', () => {
   it('should render the text that is passed in', () => {
     render(
-      <Option optionToDisplay='Test' />
-    )
+      <Router>
+        <Option optionToDisplay="Test" options={[]} />
+      </Router>
+    );
 
     const option = screen.getByText('Test');
 
