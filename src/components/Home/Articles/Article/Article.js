43aa1b4184c0a5c91c26bca40eed0b4fb9fb5188
Nick Hart <nick.hart.des@gmail.com>
2020-09-18 15:53:46 -0500

    Add intial Article setup

---
+++ src/components/Home/Articles/Article/Article.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../../../i18n';
+
+const Article = () => {
+  return (
+    <p>Article</p>
+  )
+}
+
+export default Article;
--- src/components/Home/Articles/Articles.js
+++ src/components/Home/Articles/Articles.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../../../i18n';
+import Article from './Article/Article';
 
 const Articles = () => {
   return (
-    <h2>Articles</h2>
-  )
+    <section>
+      <h2>Articles</h2>
+    </section>
+  );
 }
 
 export default Articles;
