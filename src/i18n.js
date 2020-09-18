90feda9221d2f82e841044bb93076ec8ffe59263
Nick Hart <nick.hart.des@gmail.com>
2020-09-18 12:14:14 -0500

    Move translations to i18n.init

--- src/i18n.js
+++ src/i18n.js
@@ -3,17 +3,42 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false
-    }
-  })
+// .use(Backend)
+// .use(LanguageDetector)
+
+i18n.use(initReactI18next).init({
+  fallbackLng: "en",
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: {
+    en: {
+      namespace1: {
+        interestHeader: "Choose your interests",
+        interests: {
+          business: "Business",
+          entertainment: "Entertainment",
+          health: "Health",
+          science: "Science",
+          sports: "Sports",
+          technology: "Technology",
+        },
+      },
+    },
+    es: {
+      interestHeader: "Elige tus intereses",
+      interests: {
+        business: "Los negocios",
+        entertainment: "La Entretenimiento",
+        health: "La Salud",
+        science: "La Ciencia",
+        sports: "Los Deportes",
+        technology: "La Tecnología",
+      },
+    },
+  },
+});
 
 export default i18n;
 
