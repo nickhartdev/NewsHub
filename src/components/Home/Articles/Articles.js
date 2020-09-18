1508527f4e435a57136b1294fd0d401bf1d4147b
Nick Hart <nick.hart.des@gmail.com>
2020-09-18 15:51:03 -0500

    Add initial article structure

---
+++ src/components/Home/Articles/Articles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../../i18n';
+
+const Articles = () => {
+  return (
+    <h2>Articles</h2>
+  )
+}
+
+export default Articles;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../../i18n';
+import Articles from './Articles/Articles';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
-  )
+    <main>
+      <h1>Home</h1>
+      <Articles />
+    </main>
+  );
 }
 
 export default Home;
