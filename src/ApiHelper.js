87eace5aef28389f11118995c571adb2099c8dc6
Nick Hart <nick.hart.des@gmail.com>
2020-09-18 17:04:40 -0500

    Add fetchStoriesByInterest

--- src/ApiHelper.js
+++ src/ApiHelper.js
@@ -1,10 +1,29 @@
 import apiKey from './apiKey';
 import './i18n';
 
-const baseUrl = "http://newsapi.org/v2/";
+const baseUrl = "http://newsapi.org/v2/top-headlines?";
 
-export const fetchTopHeadlinesByLanguage = async language => {
-  const response = await fetch(`${baseUrl}/top-headlines?language=${language}&apiKey=${apiKey}`);
+const buildEndpoint = interests => {
+  let endpoint = [baseUrl];
+  
+  interests.forEach(interest => {
+    endpoint.push(`category=${interest}&`);
+  })
+
+  endpoint.push(`apiKey=${apiKey}`);
+
+  return endpoint.join('');
+}
+
+export const fetchTopStoriesByLanguage = async language => {
+  const response = await fetch(`${baseUrl}language=${language}&apiKey=${apiKey}`);
+  const articles = await response.json();
+
+  return articles;
+}
+
+export const fetchStoriesByInterest = async interests => {
+  const response = await fetch(buildEndpoint(interests));
   const articles = await response.json();
 
   return articles;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import '../../i18n';
 import Articles from './Articles/Articles';
-import { fetchTopHeadlinesByLanguage } from '../../ApiHelper';
+import { fetchTopStoriesByLanguage } from '../../ApiHelper';
 
 const Home = ({ interests }) => {
   const [articles, setArticles] = useState([]);
   const {t, i18n} = useTranslation();
 
   const getAllArticles = async () => {
-    const articlesToDisplay = await fetchTopHeadlinesByLanguage(i18n.language);
-    setArticles(articlesToDisplay.articles);
+    const response = await fetchTopStoriesByLanguage(i18n.language);
+    setArticles(response.articles);
+  }
+
+  const getArticlesByInterest = async () => {
+
   }
 
   useEffect(() => {
     if (interests.length === 0) {
       // getAllArticles();
+    } else {
+
     }
   }, []);
 
