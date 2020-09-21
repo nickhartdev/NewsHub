9984d7ae5828f0ca327cb739a88dbeb629580304
Nick Hart <nick.hart.des@gmail.com>
2020-09-20 22:55:09 -0600

    Comment out old fetch functions

--- src/ApiHelper/ApiHelper.js
+++ src/ApiHelper/ApiHelper.js
@@ -21,25 +21,25 @@ export const fetchTopArticlesByLanguage = async language => {
   return articleData.articles;
 }
 
-export const fetchArticlesByInterest = async interests => {
-  const response = await fetch(buildEndpoint(interests));
-  const articleData = await response.json();
+// export const fetchArticlesByInterest = async interests => {
+//   const response = await fetch(buildEndpoint(interests));
+//   const articleData = await response.json();
 
-  return articleData.articles;
-}
+//   return articleData.articles;
+// }
 
-export const filterArticlesByLanguage = (languageArticles, interestArticles) => {
-  const sourcesByLanguage = languageArticles.map(article => article.source.id);
+// export const filterArticlesByLanguage = (languageArticles, interestArticles) => {
+//   const sourcesByLanguage = languageArticles.map(article => article.source.id);
 
-  return interestArticles.filter(article => {
-    const articleSrc = article.source.id;
-    return sourcesByLanguage.includes(articleSrc);
-  })
-}
+//   return interestArticles.filter(article => {
+//     const articleSrc = article.source.id;
+//     return sourcesByLanguage.includes(articleSrc);
+//   })
+// }
 
-export const getArticlesByLanguageAndInterests = async (language, interests) => {
-  const languageArticles = await fetchTopArticlesByLanguage(language);
-  const interestArticles = await fetchArticlesByInterest(interests);
+// export const getArticlesByLanguageAndInterests = async (language, interests) => {
+//   const languageArticles = await fetchTopArticlesByLanguage(language);
+//   const interestArticles = await fetchArticlesByInterest(interests);
 
-  return filterArticlesByLanguage(languageArticles, interestArticles);
-}
+//   return filterArticlesByLanguage(languageArticles, interestArticles);
+// }
